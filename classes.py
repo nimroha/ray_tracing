6a7b87cd07505a65173f288189061e9fa2f8a66d
nimrodm <nimrod@voyage81.com>
2022-05-02 09:12:32 +0300

    add sphere instersection, plane intersection still WIP

--- classes.py
+++ classes.py
@@ -2,6 +2,30 @@ from dataclasses import dataclass
 
 import numpy as np
 
+from utils import norm2
+
+TANGENT_TOLERANCE = 0.01
+
+@dataclass
+class Ray:
+    origin:    np.ndarray
+    direction: np.ndarray
+
+    def project(self, point):
+        """
+        projection of `point` vector on the `direction` vector
+        :param point: (x,y,z) numpy array
+        :return: length of projection
+        """
+        return np.dot(self.direction, point)
+
+    def get_point(self, t):
+        """
+        parametrized point along the ray
+        :param t: distance from origin
+        :return: (x,y,z) numyp array
+        """
+        return self.origin + t * self.direction
 
 @dataclass
 class Camera:
@@ -41,7 +65,7 @@ class Light:
 class Shape:
     material: int
 
-    def find_intersection(self, ray):
+    def find_intersection(self, ray: Ray):
         # will raise if we miss an implementation
         raise NotImplementedError(f'the subclass {self.__class__} did not implement this method')
 
@@ -51,14 +75,76 @@ class Sphere(Shape):
     center: np.ndarray
     radius: float
 
+    def find_intersection(self, ray: Ray):
+        """
+        find the ray's intersection with a sphere
+        denote:
+        o - ray origin
+        d - ray direction
+        c - sphere center
+        r - sphere radius
+        p1, p2 - intersection points
+        t1, t2 - their respective line parameters
+        a - the intersection of the ray with a perpendicular lin that passes through c
+
+        use triangle building to find the intersection point(s), if exists
+
+        :param ray: a Ray instance
+        :return: the one or two intersecting points, or False for no intersection
+        """
+        c = self.center
+        r = self.radius
+        ob_size = ray.project(c)
+        if ob_size <= 0:
+            return False # TODO is this a good "no intersection" result?
+
+        oc = c - ray.origin
+        oc_size = norm2(oc)
+        cb_size = np.sqrt(oc_size ** 2 - ob_size ** 2)
+        if np.isclose(cb_size, r, rtol=TANGENT_TOLERANCE):
+            return ray.get_point(ob_size)
+
+        if cb_size > r:
+            return False
+
+        t_diff_abs = np.sqrt(r ** 2 - cb_size ** 2)
+        return ray.get_point(cb_size - t_diff_abs), ray.get_point(cb_size + t_diff_abs)
+
 
 @dataclass
 class Plane(Shape):
     normal: np.ndarray
     offset: float
 
+    def find_intersection(self, ray: Ray):
+        """
+        find the ray's intersection with a sphere
+        denote:
+        o - ray origin
+        d - ray direction
+        n - plane normal
+        c - plane offset
+        p - the intersection point
+        t - the respective line parameter
+
+        use the plane equation <n,o + t * d> = c to find the intersection point (if exists)
+
+        :param ray: a Ray instance
+        :return: the intersecting point, or False for no intersection
+        """
+        raise NotImplementedError('not finished')
+        projection_on_normal = ray.project(self.normal)
+        if projection_on_normal <= 0:
+            return False # TODO is this a good "no intersection" result?
+
+        t = (self.offset - np.dot(ray.origin, self.normal)) / projection_on_normal
+        point = ray.get_point(t)
+
+        return point
+
 
 @dataclass
 class Box(Shape):
     center: np.ndarray
     length: float
+
--- utils.py
+++ utils.py
@@ -1,6 +1,11 @@
 import numpy as np
 import cv2 as cv
 
+from functools import partial
+
+
+norm2 = partial(np.linalg.norm, ord=2)
+
 def write_img(img, img_path):
     img = (img * 255).astype(np.uint8)
     bgr = cv.cvtColor(img, cv.COLOR_RGB2BGR)
